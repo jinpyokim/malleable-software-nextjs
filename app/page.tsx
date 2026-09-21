7b8f7b883d9d928bdcc9c404e68fbb79057b51db
jinpyokim <jinpyo.kim@gmail.com>
2026-09-20 18:00:27 -0700

    index page updated to include a new line about discovering insights from crowdsourced personal knowledge.

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
       <main aria-labelledby="brand">
         <h1 id="brand">malleable software</h1>
         <p>provides personal knowledge management with AI and</p>
-        <p>discovers insights from crowdsourced personalknowledge.</p>
+        <p>discovers insights from crowdsourced personal knowledge.</p>
         <br />
         <p>Cupertino, CA</p>
         <p><a href="mailto:reach@malleablesoft.com">reach@malleablesoft.com</a></p>
